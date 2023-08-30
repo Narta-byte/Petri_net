f0f1ab4f63a42cc98002cba24785803ecafb08c2
Viktor Kolind <81175262+Narta-byte@users.noreply.github.com>
2023-08-30 10:19:56 +0200

    Create Petri_net.py

---
+++ Petri_net.py
@@ -0,0 +1,27 @@
+class PetriNet():
+
+    def __init__(self):
+        # code here
+
+    def add_place(self, name):
+        # code here
+
+    def add_transition(self, name, id):
+        # code here
+
+    def add_edge(self, source, target):
+        # code here
+
+    def get_tokens(self, place):
+        # code here
+
+    def is_enabled(self, transition):
+        # code here
+
+    def add_marking(self, place):
+        # code here
+
+    def fire_transition(self, transition):
+        # code here
+
+# etc
